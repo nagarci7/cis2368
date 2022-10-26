0dc33906e10b70edad1f1ec1dcb8e13e1cc485af
Nicholas Garcia <87832673+nagarci7@users.noreply.github.com>
2022-10-26 14:17:42 -0500

    js stuff

---
+++ scripts.js
@@ -0,0 +1,20 @@
+console.log('hello world');
+
+const btn = document.querySelector('#button');
+
+
+const cursorSmall = document.querySelector('.small');
+const cursorBig = document.querySelector('.big');
+
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', positionElement)
